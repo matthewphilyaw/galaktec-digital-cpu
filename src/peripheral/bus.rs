06e9641746f7019300557d5eaa737db6b1ab1ac3
Matthew Philyaw <matthew.philyaw@gmail.com>
2020-10-03 13:39:10 -0400

    More tests for bus.

--- src/peripheral/bus.rs
+++ src/peripheral/bus.rs
@@ -173,11 +173,11 @@ pub trait BussedPeripheral {
 mod tests {
     use super::*;
 
-    pub struct TestBusPeripheral {
+    pub struct SimpleTestBusPeripheral {
         value: Option<u32>,
     }
 
-    impl BussedPeripheral for TestBusPeripheral {
+    impl BussedPeripheral for SimpleTestBusPeripheral {
         fn write(&mut self, _address: u32, value: u32) {
             self.value = Some(value);
         }
@@ -186,18 +186,73 @@ mod tests {
         }
     }
 
+    pub struct WordBusPeripheral {
+        storage: Vec<u32>,
+    }
+
+    impl WordBusPeripheral {
+        fn new(initial_values: Vec<u32>) -> Self {
+            WordBusPeripheral {
+                storage: initial_values
+            }
+        }
+    }
+
+    impl BussedPeripheral for WordBusPeripheral {
+        fn write(&mut self, address: u32, value: u32) {
+            self.storage[address as usize] = value;
+        }
+
+        fn read(&mut self, address: u32) -> u32 {
+            self.storage[address as usize]
+        }
+    }
+
+    fn create_word_bus_peripheral_with_address(initial_value: Vec<u32>, address: u32) -> BusPeripheral {
+        let length = initial_value.len() - 1;
+        let bussed_peripheral = WordBusPeripheral {
+            storage: initial_value,
+        };
+
+        BusPeripheral::new(
+            address,
+            address + (length as u32),
+            1,
+            1,
+            Box::new(bussed_peripheral),
+        )
+    }
+
+    // End address is always four words past start
+    fn create_bus_peripheral_with_start_address(
+        address: u32,
+        initial_value: Option<u32>
+    ) -> BusPeripheral {
+        let bussed_peripheral = SimpleTestBusPeripheral {
+            value: initial_value,
+        };
+
+        BusPeripheral::new(
+            address,
+            address + 3,
+            1,
+            1,
+            Box::new(bussed_peripheral),
+        )
+    }
+
     fn create_simple_bus_peripheral(
         initial_value: Option<u32>,
         write_latency: usize,
         read_latency: usize,
     ) -> BusPeripheral {
-        let bussed_peripheral = TestBusPeripheral {
+        let bussed_peripheral = SimpleTestBusPeripheral {
             value: initial_value,
         };
 
         BusPeripheral::new(
             0,
-            4,
+            3,
             write_latency,
             read_latency,
             Box::new(bussed_peripheral),
@@ -368,4 +423,74 @@ mod tests {
         clock_bus(&mut bus);
         assert_eq!(bus.idle(), true);
     }
+
+    #[test]
+    fn can_write_to_two_different_peripherals() {
+        let bus_periph_1 = create_bus_peripheral_with_start_address(0, None);
+        let bus_periph_2 = create_bus_peripheral_with_start_address(4, None);
+
+        let mut bus = Bus::new(vec![
+            bus_periph_1,
+            bus_periph_2
+        ]);
+
+        bus.write(0, 1234);
+        clock_bus(&mut bus);
+
+        bus.write(4, 4567);
+        clock_bus(&mut bus);
+
+        let periph1_val = bus.bus_peripherals[0].bussed_peripheral.read(0);
+        let periph2_val = bus.bus_peripherals[1].bussed_peripheral.read(0);
+
+        assert_eq!(periph1_val, 1234);
+        assert_eq!(periph2_val, 4567);
+    }
+
+    #[test]
+    fn can_read_from_two_different_peripherals() {
+        let bus_periph_1 = create_bus_peripheral_with_start_address(0, Some(1234));
+        let bus_periph_2 = create_bus_peripheral_with_start_address(4, Some(4567));
+
+        let mut bus = Bus::new(vec![
+            bus_periph_1,
+            bus_periph_2
+        ]);
+
+        bus.read(0);
+        clock_bus(&mut bus);
+
+        let periph1_val = bus.result;
+
+        bus.read(4);
+        clock_bus(&mut bus);
+
+        let periph2_val = bus.result;
+
+        assert_eq!(periph1_val, 1234);
+        assert_eq!(periph2_val, 4567);
+    }
+
+    #[test]
+    fn can_write_to_multiple_addresses_within_peripheral() {
+        let periph = create_word_bus_peripheral_with_address(
+            vec![0; 2],
+            0,
+        );
+
+        let mut bus = Bus::new(vec![periph]);
+
+        bus.write(0, 12345);
+        clock_bus(&mut bus);
+
+        bus.write(1, 54321);
+        clock_bus(&mut bus);
+
+        let write_1 = bus.bus_peripherals[0].bussed_peripheral.read(0);
+        let write_2 = bus.bus_peripherals[0].bussed_peripheral.read(1);
+
+        assert_eq!(write_1, 12345);
+        assert_eq!(write_2, 54321);
+    }
+
 }
