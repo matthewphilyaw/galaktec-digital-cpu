fdfc15618467189c5bf79ccb300b2dd32b25640b
Matthew Philyaw <matthew.philyaw@gmail.com>
2020-10-31 10:17:29 -0400

    Another pass at the IO layer of the CPU. Looking for a base to build the rest of hardware on.

--- galaktec-digital-cpu-core/src/clock.rs
+++ galaktec-digital-cpu-core/src/clock.rs
@@ -1,16 +1,17 @@
-use crate::DiscreteDevice;
 use std::cell::RefCell;
 use std::rc::Rc;
 
-type Discrete = RefCell<dyn DiscreteDevice>;
+use crate::discrete::Discrete;
+
+type DiscreteDevice = Rc<RefCell<dyn Discrete>>;
 
 #[derive(Debug)]
 pub struct GenericClock {
-    discrete_items: Vec<Rc<Discrete>>,
+    discrete_items: Vec<DiscreteDevice>,
 }
 
 impl GenericClock {
-    pub fn new(discrete_items: Vec<Rc<Discrete>>) -> Self {
+    pub fn new(discrete_items: Vec<DiscreteDevice>) -> Self {
         GenericClock { discrete_items }
     }
 
@@ -20,7 +21,7 @@ impl GenericClock {
         }
 
         for ref mut di in self.discrete_items.iter() {
-            di.borrow_mut().settle();
+            di.borrow_mut().process_input();
         }
 
         for ref mut di in self.discrete_items.iter() {
