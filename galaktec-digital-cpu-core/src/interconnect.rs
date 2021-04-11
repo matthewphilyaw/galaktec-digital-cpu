a3b9a26a730d8e3ad6909f48f1bff96166ecb839
Matthew Philyaw <matthew.philyaw@gmail.com>
2021-04-11 00:12:58 -0400

    Refactor interconnect to eliminate traits.

--- galaktec-digital-cpu-core/src/interconnect.rs
+++ galaktec-digital-cpu-core/src/interconnect.rs
@@ -1,25 +1,9 @@
 use std::fmt::Debug;
-
-pub trait Peripheral<Input, Output>: Debug
-where
-    Input: Debug + Clone + PartialEq,
-    Output: Debug + Clone + PartialEq,
-{
-    fn transmit(&mut self, command: Input);
-    fn receive(&mut self) -> Option<Output>;
-}
-
-pub trait Controller<Input, Output>: Debug
-where
-    Input: Debug + Clone + PartialEq,
-    Output: Debug + Clone + PartialEq,
-{
-    fn receive(&mut self) -> Option<Input>;
-    fn transmit(&mut self, output: Output);
-}
+use std::rc::Rc;
+use std::cell::RefCell;
 
 #[derive(Debug)]
-pub struct Interconnect<Input, Output>
+struct Interconnect<Input, Output>
 where
     Input: Debug + Clone + PartialEq,
     Output: Debug + Clone + PartialEq,
@@ -28,43 +12,66 @@ where
     output: Option<Output>,
 }
 
-impl<Input, Output> Interconnect<Input, Output>
+#[derive(Debug)]
+pub struct Peripheral<Input, Output>
 where
     Input: Debug + Clone + PartialEq,
     Output: Debug + Clone + PartialEq,
 {
-    pub fn new() -> Self {
-        Interconnect {
-            input: None,
-            output: None,
-        }
+    interconnect: Rc<RefCell<Interconnect<Input, Output>>>
+}
+
+impl <Input, Output> Peripheral<Input, Output>
+where
+    Input: Debug + Clone + PartialEq,
+    Output: Debug + Clone + PartialEq,
+{
+    pub fn transmit(&mut self, input: Input) {
+        self.interconnect.as_ref().borrow_mut().input = Some(input);
+    }
+
+    pub fn receive(&mut self) -> Option<Output> {
+        std::mem::replace(&mut self.interconnect.borrow_mut().output, None)
     }
 }
 
-impl<Input, Output> Peripheral<Input, Output> for Interconnect<Input, Output>
+
+#[derive(Debug)]
+pub struct Controller<Input, Output>
 where
     Input: Debug + Clone + PartialEq,
     Output: Debug + Clone + PartialEq,
 {
-    fn transmit(&mut self, input: Input) {
-        self.input = Some(input);
+    interconnect: Rc<RefCell<Interconnect<Input, Output>>>
+}
+
+
+impl<Input, Output> Controller<Input, Output>
+where
+    Input: Debug + Clone + PartialEq,
+    Output: Debug + Clone + PartialEq,
+{
+    pub fn receive(&mut self) -> Option<Input> {
+        std::mem::replace(&mut self.interconnect.borrow_mut().input, None)
     }
 
-    fn receive(&mut self) -> Option<Output> {
-        std::mem::replace(&mut self.output, None)
+    pub fn transmit(&mut self, output: Output) {
+        self.interconnect.borrow_mut().output = Some(output);
     }
 }
 
-impl<Input, Output> Controller<Input, Output> for Interconnect<Input, Output>
+pub fn interconnect<Input, Output>() -> (Controller<Input, Output>, Peripheral<Input, Output>)
 where
     Input: Debug + Clone + PartialEq,
     Output: Debug + Clone + PartialEq,
 {
-    fn receive(&mut self) -> Option<Input> {
-        std::mem::replace(&mut self.input, None)
-    }
+    let interconnect = Rc::new(RefCell::new(Interconnect {
+        input: None,
+        output: None,
+    }));
 
-    fn transmit(&mut self, output: Output) {
-        self.output = Some(output);
-    }
+    (
+        Controller { interconnect: interconnect.clone() },
+        Peripheral { interconnect },
+    )
 }
--- galaktec-digital-cpu-core/tests/integration_test.rs
+++ galaktec-digital-cpu-core/tests/integration_test.rs
@@ -1,8 +1,8 @@
 use std::cell::RefCell;
 use std::ops::DerefMut;
-use std::rc::{Rc};
+use std::rc::Rc;
 
-use galaktec_digital_cpu_core::{Broadcast, Clock, Controller, Interconnect, Peripheral, Update};
+use galaktec_digital_cpu_core::{Broadcast, Clock, Update, interconnect, Controller, Peripheral};
 
 /* -------------- Counter Peripheral ---------------------- */
 
@@ -15,11 +15,11 @@ enum CounterOperation {
 #[derive(Debug)]
 struct CounterPeripheral {
     count: usize,
-    controller: Rc<RefCell<dyn Controller<CounterOperation, usize>>>,
+    controller: Controller<CounterOperation, usize>,
 }
 
 impl CounterPeripheral {
-    fn new(controller: Rc<RefCell<dyn Controller<CounterOperation, usize>>>) -> Self {
+    fn new(controller: Controller<CounterOperation, usize>) -> Self {
         CounterPeripheral {
             count: 0,
             controller,
@@ -29,8 +29,7 @@ impl CounterPeripheral {
 
 impl Update for CounterPeripheral {
     fn update(&mut self) {
-        let mut controller = self.controller.as_ref().borrow_mut();
-        let input = controller.receive();
+        let input = self.controller.receive();
         self.count = if let Some(op) = input {
             match op {
                 CounterOperation::Set(new_value) => new_value,
@@ -40,7 +39,7 @@ impl Update for CounterPeripheral {
             self.count + 1
         };
 
-        controller.transmit(self.count)
+        self.controller.transmit(self.count)
     }
 }
 
@@ -50,29 +49,31 @@ impl Update for CounterPeripheral {
 struct CounterResetPeripheral {
     trigger_at: usize,
     set_to: usize,
-    counter_peripheral: Rc<RefCell<dyn Peripheral<CounterOperation, usize>>>,
+    counter_peripheral: Peripheral<CounterOperation, usize>,
+    observed_count: Option<usize>,
 }
 
 impl CounterResetPeripheral {
     fn new(
         trigger_at: usize,
         set_to: usize,
-        counter_peripheral: Rc<RefCell<dyn Peripheral<CounterOperation, usize>>>,
+        counter_peripheral: Peripheral<CounterOperation, usize>,
     ) -> Self {
         CounterResetPeripheral {
             trigger_at,
             set_to,
             counter_peripheral,
+            observed_count: None
         }
     }
 }
 
 impl Broadcast for CounterResetPeripheral {
     fn broadcast(&mut self) {
-        let mut peripheral = self.counter_peripheral.as_ref().borrow_mut();
-        if let Some(current_count) = peripheral.receive() {
+        self.observed_count = self.counter_peripheral.receive();
+        if let Some(current_count) = self.observed_count {
             if current_count == self.trigger_at {
-                peripheral.transmit(CounterOperation::Set(self.set_to));
+                self.counter_peripheral.transmit(CounterOperation::Set(self.set_to));
             }
         }
     }
@@ -80,29 +81,31 @@ impl Broadcast for CounterResetPeripheral {
 
 #[test]
 fn counter_test() {
-    let counter_interconnect: Rc<RefCell<Interconnect<CounterOperation, usize>>> =
-        Rc::new(RefCell::new(Interconnect::new()));
+    let (c, p) = interconnect();
     let counter = Rc::new(RefCell::new(CounterPeripheral::new(
-        counter_interconnect.clone(),
+        c
     )));
+
     let counter_reset = Rc::new(RefCell::new(CounterResetPeripheral::new(
         10,
         20,
-        counter_interconnect.clone(),
+        p,
     )));
 
-    let mut clock = Clock::new(vec![counter_reset], vec![counter]);
+    let mut clock = Clock::new(vec![counter_reset.clone()], vec![counter]);
 
-    for n in 0..11 {
+    for n in 0..12 {
         clock.step();
 
-        let count =
-            Peripheral::receive(counter_interconnect.as_ref().borrow_mut().deref_mut()).unwrap();
-
-        if n == 11 {
-            assert_eq!(count, 20);
-        } else {
-            assert_eq!(count, n + 1);
+        match counter_reset.borrow().observed_count {
+            Some(count) => {
+                if n == 11 {
+                    assert_eq!(count, 20);
+                } else {
+                    assert_eq!(count, n);
+                }
+            },
+            _ => continue
         }
     }
 }
